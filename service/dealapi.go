0a7f23ced70ee3bffe7a24633ed419cbcafbf476
tpsmpc <wyhx04@gmail.com>
2024-04-23 19:43:52 +0800

    fix time lock check

--- service/dealapi.go
+++ service/dealapi.go
@@ -33,14 +33,9 @@ func SendTxEssence(signAcc string, txEssenceBytes []byte) ([]byte, error) {
 	if len(essence.Outputs) < 1 {
 		return nil, fmt.Errorf("illegal essence outputs(%d)", len(essence.Outputs))
 	}
-	if !verifyMsgOutput(proxy.Smr, essence.Outputs[0]) {
-		return nil, fmt.Errorf("illegal essence output(0)")
-	}
-	if len(essence.Outputs) > 1 {
-		for i := 1; i < len(essence.Outputs); i++ {
-			if !verifySendBackOutput(proxy.Smr, essence.Outputs[i]) {
-				return nil, fmt.Errorf("illegal essence output(%d)", i)
-			}
+	for i := 0; i < len(essence.Outputs); i++ {
+		if !verifyMsgOutput(proxy.Smr, essence.Outputs[i]) {
+			return nil, fmt.Errorf("illegal essence output(%d)", i)
 		}
 	}
 
@@ -93,10 +88,6 @@ func verifyMsgOutput(to string, op iotago.Output) bool {
 		return false
 	}
 	return true
-}
-
-func verifySendBackOutput(to string, op iotago.Output) bool {
-	return verifyBalanceOutput(to, op) && (!op.UnlockConditionSet().HasTimelockCondition())
 }
 
 func verifyBalanceOutput(to string, op iotago.Output) bool {
