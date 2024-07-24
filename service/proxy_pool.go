6eeccac7001613cb9d8987753f5dd8d1ce91cd6a
tpsmpc <wyhx04@gmail.com>
2024-07-24 19:51:12 +0800

    fix service

--- service/proxy_pool.go
+++ service/proxy_pool.go
@@ -25,32 +25,6 @@ func RunKeepProxyPoolFull() {
 }
 
 func RunCheckProxyPoolBalance() {
-	ticker := time.NewTicker(time.Hour * time.Duration(config.ProxyBalanceCheckHours))
-	for range ticker.C {
-		addrs, err := model.GetProxyPool(model.USED_ADDRESS)
-		if err != nil {
-			gl.OutLogger.Error("model.GetUsedProxyPool error. %v", err)
-		}
-
-		for bech32Addr, enpk := range addrs {
-			time.Sleep(time.Second * 5)
-			if !wallet.ChecKEd25519Addr(enpk, bech32Addr) {
-				continue
-			}
-
-			w := wallet.NewIotaSmrWallet(config.ShimmerRpc, bech32Addr, enpk, "")
-
-			id, err := w.Recycle(time.Now().Unix() - config.RecycleMsgTime)
-			if err != nil {
-				gl.OutLogger.Error("w.Recycle error. %s : %v", bech32Addr, err)
-				continue
-			}
-			gl.OutLogger.Error("%s recycle msg, blockid : %s", bech32Addr, hex.EncodeToString(id))
-		}
-	}
-}
-
-func RunRecycleMsgOutputs() {
 	w := wallet.NewIotaSmrWallet(config.ShimmerRpc, "", "", "")
 	ticker := time.NewTicker(time.Hour * time.Duration(config.ProxyBalanceCheckHours))
 	for range ticker.C {
@@ -76,6 +50,32 @@ func RunRecycleMsgOutputs() {
 					gl.OutLogger.Error("model.InsertSendSmrOrder error. %s : %v", bech32Addr, err)
 				}
 			}
+		}
+	}
+}
+
+func RunRecycleMsgOutputs() {
+	ticker := time.NewTicker(time.Hour * time.Duration(config.ProxyBalanceCheckHours))
+	for range ticker.C {
+		addrs, err := model.GetProxyPool(model.USED_ADDRESS)
+		if err != nil {
+			gl.OutLogger.Error("model.GetUsedProxyPool error. %v", err)
+		}
+
+		for bech32Addr, enpk := range addrs {
+			time.Sleep(time.Second * 5)
+			if !wallet.ChecKEd25519Addr(enpk, bech32Addr) {
+				continue
+			}
+
+			w := wallet.NewIotaSmrWallet(config.ShimmerRpc, bech32Addr, enpk, "")
+
+			id, err := w.Recycle(time.Now().Unix() - config.RecycleMsgTime)
+			if err != nil {
+				gl.OutLogger.Error("w.Recycle error. %s : %v", bech32Addr, err)
+				continue
+			}
+			gl.OutLogger.Error("%s recycle msg, blockid : %s", bech32Addr, hex.EncodeToString(id))
 		}
 	}
 }
